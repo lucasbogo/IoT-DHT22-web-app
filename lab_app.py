b7d9532e2332661e7ea1a813fd6c875e5b9441e7
root <github@lucasbogo.com>
2022-12-05 13:45:33 -0300

    google charts API working fine!

--- lab_app.py
+++ lab_app.py
@@ -16,22 +16,23 @@ def hello():
 def lab_temp():
 	humidity, temperature = Adafruit_DHT.read_retry(Adafruit_DHT.AM2302, 17)
 	if humidity is not None and temperature is not None:
-		return render_template("lab_temp.html",temp=temperature, hum=humidity)
+		return render_template("lab_temp.html",temp=temperature,hum=humidity)
 	else:
 		return render_template("no_sensor.html")
 
 @app.route("/lab_env_db", methods=['GET']) 
 def lab_env_db():
 	temperatures, humidities, from_date_str, to_date_str = get_records()
-	return render_template("lab_env_db.html",temp=temperatures,hum=humidities,  temp_items=len(temperatures), hum_items=len(humiddities))
+	#return render_template("lab_env_db.html",temp=temperatures,hum=humidities)
+	return render_template("lab_env_db.html",temp 	= temperatures,hum= humidities,	temp_items= len(temperatures),hum_items= len(humidities))
 
 def get_records():
 	from_date_str 	= request.args.get('from',time.strftime("%Y-%m-%d 00:00")) #Get the from date value from the URL
 	to_date_str 	= request.args.get('to',time.strftime("%Y-%m-%d %H:%M"))   #Get the to date value from the URL
-	range_h_form	= request.args.get('range_h','');  #This will return a string, if field range_h exists in the request	
-	
+	range_h_form	= request.args.get('range_h','');  #This will return a string, if field range_h exists in the request
+
 	range_h_int 	= "nan"  #initialise this variable with not a number
-	
+
 	try: 
 		range_h_int	= int(range_h_form)
 	except:
@@ -47,8 +48,8 @@ def get_records():
 		time_now		= datetime.datetime.now()
 		time_from 		= time_now - datetime.timedelta(hours = range_h_int)
 		time_to   		= time_now
-		from_date_str  	        = time_from.strftime("%Y-%m-%d %H:%M")
-		to_date_str	        = time_to.strftime("%Y-%m-%d %H:%M")		
+		from_date_str   = time_from.strftime("%Y-%m-%d %H:%M")
+		to_date_str	    = time_to.strftime("%Y-%m-%d %H:%M")
 
 	conn=sqlite3.connect('/var/www/lab_app/lab_app.db')
 	curs=conn.cursor()
@@ -68,4 +69,3 @@ def validate_date(d):
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8080)
-
